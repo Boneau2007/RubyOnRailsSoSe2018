ffdc124106765810397b686bf97dd363cf471d3e
Huber <HuberA@de.hydac.int>
2018-09-10 15:37:14 +0200

    Added associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,6 @@
 class Answer < ApplicationRecord
+  belongs_to :userprofile
   belongs_to :question 
+  #An anwer has many Comments
+  has_many :comments, class_name: "Answer"
 end
--- app/models/userprofile.rb
+++ app/models/userprofile.rb
@@ -1,5 +1,7 @@
 class Userprofile < ApplicationRecord
   belongs_to :user
+  has_many :answers
+  has_many :questions
   validate :name, presence: true
   validate :firstname, presence: true
 end
