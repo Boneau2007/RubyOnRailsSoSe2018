cf721960988dc85d6b1a3a66b2e9d77470b0f96d
Huber <HuberA@de.hydac.int>
2018-09-12 14:19:55 +0200

    ansichten umgbaut

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
-  def index
-  end
+	
+	before_action :get_data, only: [:index, :show]
+	# GET /questions
+	def index 
+		@questions = Question.all
+		@question = @questions.find(params[:questions_id])
+	end
 end
